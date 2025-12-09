1f177d3556193ac3575e3afb678f1d5703b5ed9c
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-12-09 12:59:05 +0530

    modified

--- test_product_details.py
+++ test_product_details.py
@@ -1,8 +1,11 @@
 def test_product_details():
-    expected_output = (
-        "Product Id: 101\n"
-        "Prodcut name: iPhone\n"
-        "Quantity: 1\n"
-        "Price: 155000\n"
-    )
+expected_output = (
+    "Product Id: 101\n"
+    "Prodcut name: iPhone\n"
+    "Quantity: 1\n"
+    "Price: 155000\n"
+)
     assert product_details(101, "iPhone", 1, 155000) == expected_output
+
+
+
