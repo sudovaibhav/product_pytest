e2534a9ca3985bb9472830e6e5454cc5756a6a0c
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-12-09 13:00:54 +0530

    modified

--- test_product_details.py
+++ test_product_details.py
@@ -1,11 +1,10 @@
+from products import product_details
+
 def test_product_details():
-expected_output = (
-    "Product Id: 101\n"
-    "Prodcut name: iPhone\n"
-    "Quantity: 1\n"
-    "Price: 155000\n"
-)
+    expected_output = (
+        "Product Id: 101\n"
+        "Prodcut name: iPhone\n"
+        "Quantity: 1\n"
+        "Price: 155000\n"
+    )
     assert product_details(101, "iPhone", 1, 155000) == expected_output
-
-
-
