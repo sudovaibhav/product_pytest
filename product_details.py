210c7811455ce2301599c0581f1c24c01f99a8ca
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-12-09 13:05:02 +0530

    modified

---
+++ product_details.py
@@ -0,0 +1,11 @@
+def product_details(product_id, name, quantity, price):
+    result = (
+        f"Product Id: {product_id}\n"
+        f"Prodcut name: {name}\n"
+        f"Quantity: {quantity}\n"
+        f"Price: {price}\n"
+    )
+    return result
+
+if __name__ == "__main__":
+    print(product_details(101, "iPhone", 1, 155000))
